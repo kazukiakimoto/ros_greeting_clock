df9ed845b640339cb115b0dd2ff80bc45618abcd
Ubuntu <ubuntu@ubuntu.mshome.net>
2020-12-29 10:51:46 +0000

    count -> realtime

--- scripts/count.py
+++ scripts/count.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
+import datetime
+import pytz
 
 rospy.init_node('count')
 pub = rospy.Publisher('count_up', Int32, queue_size=1)
 rate = rospy.Rate(1)
-n = 0
 while not rospy.is_shutdown():
-    n += 1
-    pub.publish(n)
-    if n > 22:
-        n = -1
+    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+    #now = datetime.datetime.now()
+    hour = int(now.strftime('%H'))
+    pub.publish(hour)
     rate.sleep()
